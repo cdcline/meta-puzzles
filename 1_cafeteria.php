89f4ab397b3fcc39fd1c41dfa6f6ef4b72aa4bbe
Chris Cline <1248182+cdcline@users.noreply.github.com>
2022-05-16 16:55:45 -0700

    Stop using linked lists
    No fun, just cold hard calculations.
    
    Still missing some part of the forumla but at least
    we're running in the right timeframes now.

--- 1_cafeteria.php
+++ 1_cafeteria.php
@@ -1,167 +1,46 @@
 <?php
-class Seat {
-   private $position;
-   private $isSeated;
-   private $nextSeat;
-   private $prevSeat;
-   private $startSeat= false;
-   private $numSpaces;
-
-   public function __construct($positon, $isSeated, $numSpaces) {
-      $this->position = $positon;
-      $this->isSeated = $isSeated;
-      $this->numSpaces = $numSpaces;
-   }
-
-   public function setNextSeat(&$nextSeat) {
-      $this->nextSeat = $nextSeat;
-   }
-
-   public function setPrevSeat(&$prevSeat) {
-      $this->prevSeat = $prevSeat;
-   }
-
-   public function getPosition() {
-      return $this->position;
-   }
-
-   public function getNextSeat() {
-      return $this->nextSeat;
-   }
-
-   public function getPrevSeat() {
-      return $this->prevSeat;
-   }
-
-   public function isSeated() {
-      return $this->isSeated;
-   }
-
-   public function setStart() {
-      $this->startSeat = true;
-   }
-
-   public function isStart() {
-      return $this->startSeat;
-   }
-
-   private function getCloseSeat(bool $goingUp) {
-      return $goingUp ? $this->getNextSeat() : $this->getPrevSeat();
-   }
-
-   public function getFreeSeat(int $numSpaces, bool $goingUp = true) {
-      $spaceToNext = 0;
-      $seat = $this->getCloseSeat($goingUp);
-      if ($seat->isSeated()) {
-         return false;
-      }
-
-      while ($spaceToNext++ < $numSpaces) {
-         $seat = $seat->getCloseSeat($goingUp);
-         if ($seat->isSeated()) {
-            return false;
-         }
-      }
-      return $seat;
-   }
-
-   public function getNextSpaceToSit(): ?self {
-      $spaceToNext = 0;
-      $seat = $this;
-      while ($seat = $seat->getNextSeat()) {
-         if ($seat->isStart()) {
-            return null;
-         }
-         if ($seat->isSeated()) {
-            $spaceToNext = 0;
-            continue;
-         }
-         if (++$spaceToNext > $this->numSpaces) {
-            return $seat;
-         }
-      }
-   }
-
-   public function hasEmptySeats(int $numSpaces, bool $goingUp = true) {
-      return $this->getFreeSeat(($numSpaces - 1), $goingUp);
-   }
-
-   public function hasEnoughSpace(int $numSpaces) {
-      return $this->hasEmptySeats($numSpaces, true);
-   }
-
-   public function sitDown() {
-      $this->isSeated = true;
-   }
-
-   public function print() {
-      echo "P:{$this->getPosition()}, Se:{$this->isSeated()}, St:{$this->isStart()}\n";
-   }
-}
 
 class TableSeating {
-   private $iSeats;
+   private $numSeats;
+   private $alreadySat;
    private $seatSpace;
 
    public function __construct($numSeats, $seatSpace, $alreadySat) {
+      $this->numSeats = $numSeats;
+      sort($alreadySat, SORT_NUMERIC);
       $this->alreadySat = $alreadySat;
+
       $this->seatSpace = $seatSpace;
-      $i = 0;
-      $aSat = array_flip($alreadySat);
-
-      while (++$i <= $numSeats) {
-         $isSat = isset($aSat[$i]);
-         $this->iSeats[$i] = new Seat($i, $isSat, $this->seatSpace);
-      }
-
-      foreach ($this->iSeats as $i => $seat) {
-         $nextSeat = $i + 1;
-         $prevSeat = $i - 1;
-         if ($prevSeat < 1) {
-            $prevSeat = $numSeats;
-         }
-         if ($nextSeat > $numSeats) {
-            $nextSeat = 1;
-         }
-         $seat->setNextSeat($this->iSeats[$nextSeat]);
-         $seat->setPrevSeat($this->iSeats[$prevSeat]);
-      }
    }
 
-   private function startSittingAroundSeat($seatNumber) {
-      $startSeat = $this->iSeats[$seatNumber];
-      $startSeat->setStart();
-      $iSeat = $startSeat;
-      while ($iSeat = $iSeat->getNextSpaceToSit()) {
-         if ($iSeat->hasEnoughSpace($this->seatSpace)) {
-            $iSeat->sitDown();
-         }
-      }
+   private function getMostPossibleSeats(): int {
+         return $this->numSeats / ($this->seatSpace + 1);
+   }
+
+   private function getMostSeatsInSpace(int $spaceToFill): int {
+      return ($spaceToFill - $this->seatSpace) / ($this->seatSpace + 1);
    }
 
    public function getMostNewSeats(): int {
-      $minSpaceToSeatMore = count($this->iSeats) / 2;
-      if ($this->seatSpace > $minSpaceToSeatMore) {
+      if ($this->seatSpace >= ($this->numSeats / 2)) {
          return 0;
       }
-
-      $this->startSittingAroundSeat($this->alreadySat[0]);
-
-      $totalSat = 0;
-      foreach ($this->iSeats as $seat) {
-         if ($seat->isSeated()) {
-            $totalSat++;
+      if (!$this->alreadySat) {
+         return $this->getMostPossibleSeats();
+      } else {
+         $totalNewSeats = 0;
+         for ($i = 0; $i < count($this->alreadySat); $i++) {
+            $iNextSeat = $i + 1;
+            $distanceToFill = 0;
+            if(isset($this->alreadySat[$iNextSeat])) {
+               $distanceToFill = $this->alreadySat[$iNextSeat] - $this->alreadySat[$i] - 1;
+            } else {
+               $distanceToFill = ($this->numSeats - $this->alreadySat[$i]) + $this->alreadySat[0] - 1;
+            }
+            $totalNewSeats += $this->getMostSeatsInSpace($distanceToFill);
          }
       }
-
-      return $totalSat - count($this->alreadySat);
-   }
-
-   public function printSeats() {
-      echo "Seats ({$this->seatSpace}):\n";
-      foreach ($this->iSeats as $i) {
-         echo "\t{$i->getPosition()}: {$i->isSeated()}\n";
-      }
+      return $totalNewSeats;
    }
 }
 
@@ -172,12 +51,31 @@ function getMaxAdditionalDinersCount(int $N, int $K, int $M, array $S): int {
 }
 
 $testCases = [
+['data' => ['N' => 10, 'K' => 1, 'M' => 0, 'S' => []],
+ 'Val' => 5
+],
+['data' => ['N' => 10, 'K' => 4, 'M' => 0, 'S' => []],
+ 'Val' => 2
+],
+['data' => ['N' => 10, 'K' => 4, 'M' => 0, 'S' => [4]],
+ 'Val' => 1
+],
+['data' => ['N' => 10, 'K' => 5, 'M' => 0, 'S' => []],
+ 'Val' => 0
+],
+['data' => ['N' => 10, 'K' => 1, 'M' => 1, 'S' => [1]],
+ 'Val' => 4
+],
+['data' => ['N' => 10, 'K' => 1, 'M' => 2, 'S' => [1, 4]],
+ 'Val' => 2
+],
 ['data' => ['N' => 10, 'K' => 1, 'M' => 2, 'S' => [2, 6]],
  'Val' => 3
 ],
 ['data' => ['N' => 15, 'K' => 2, 'M' => 3, 'S' => [11, 6, 14]],
  'Val' => 1
-]];
+]
+];
 
 $testFunc = function($tData): ?string {
    try {
